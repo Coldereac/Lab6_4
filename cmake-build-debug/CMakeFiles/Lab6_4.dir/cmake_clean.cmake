08664b9d0dfc445ca9de0f8a59fd192d4b5991d5
Anton Malynevskyi <malina.elfg@gmail.com>
2024-04-09 11:28:10 +0200

    make string output

---
+++ cmake-build-debug/CMakeFiles/Lab6_4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab6_4.dir/main.c.o"
+  "CMakeFiles/Lab6_4.dir/main.c.o.d"
+  "Lab6_4"
+  "Lab6_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab6_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
